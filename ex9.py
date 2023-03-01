1c84aa61dcd78354abad9ab639bc6b1141145e0c
Gaspary23 <pgaspary23@gmail.com>
2023-03-01 13:34:51 -0300

    fix: ex9

--- ex9.py
+++ ex9.py
@@ -1,11 +1,14 @@
 def cartesian_product(lst, n):
     if n == 1:
         return lst
-    return [x + y for y in cartesian_product(lst, n - 1) for x in lst]
+    
+    return [let + next_lets \
+                for next_lets in cartesian_product(lst, n - 1) \
+                for let in lst]
 
 def main():
     n = int(input("Enter n: "))
-    lst = [chr(x) for x in range(65, 65 + n)]
+    lst = [chr(let) for let in range(65, 65 + n)]
     
     print (cartesian_product(lst, n))
 
